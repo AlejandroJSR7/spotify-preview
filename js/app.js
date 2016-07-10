0c0c0ff0f4a03957feb848fd59c74ef7b603b363
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-07-10 04:32:05 -0500

    Add API Spotify

--- js/app.js
+++ js/app.js
@@ -1,92 +1,76 @@
 $(document).ready(function(){
-
+  //Rotal palabras
   $("#rotate-text").textrotator({
     animation: "flip",
     separator: ",",
     speed: 3000
   });
 
-  var listenOption_Artist = $('#listenOption_Artist'),
-      listenOption_Album = $('#listenOption_Album'),
-      listenOption_Track = $('#listenOption_Track'),
-      listenOption_Playlist = $('#listenOption_Playlist');
+  //Enfocar el cursor en el input
+  $(".inputSearch.active").focus();
 
-  var inputSearch_Artist = $('#inputSearch_Artist'),
-      inputSearch_Album = $('#inputSearch_Album'),
-      inputSearch_Track = $('#inputSearch_Track'),
-      inputSearch_Playlist = $('#inputSearch_Playlist');
+  //Variables de las opciones a buscar
+  var listenOption = $(".list-options li");
+  var listenOption_Track = $("#listenOption_Track");
+  var listenOption_Artist = $("#listenOption_Artist");
+  var listenOption_Album = $("#listenOption_Album");
+  var listenOption_Playlist = $("#listenOption_Playlist");
 
-  listenOption_Artist.on('click', function(){
-    if(!$(this).hasClass('active')){
-      $(this).addClass('active');
-      $(this).siblings().removeClass('active');
+  //Variable del campo de busqueda
+  var inputSearch_Name = $("#inputSearch_Name");
 
-      inputSearch_Artist.addClass('active');
-      inputSearch_Artist.siblings().removeClass('active');
+  //Variable que nos indicará que opción buscar (track, artist, album, playlist)
+  var valueOfOption = $(".list-options li.active").attr("data-option");
+
+  //Variable que guarda el contenido del input Seach
+  var nameOfSearch;
+
+  //En esta variable guardaré la URL con la que realizaré el llamado AJAX
+  var urlToFindSomething;
+
+
+  listenOption.on("click", function(){
+    //Si una opción de busqueda no tiene la clase active se le agrega, y se quita a los demás.
+    if(!$(this).hasClass("active")){
+      $(this).addClass("active");
+      $(this).siblings().removeClass("active");
     }
+
+    //Agregamos el data option de la opción que se selecciono
+    valueOfOption = $(this).attr("data-option");
+
+    //Se agrega el data-option como valor al data-search del campo de busqueda
+    inputSearch_Name.attr("data-search", ""+valueOfOption+"");
+    inputSearch_Name.attr("placeholder", "Search "+valueOfOption+"");
+
+    console.log(valueOfOption);
   });
 
-  listenOption_Album.on('click', function(){
-    if(!$(this).hasClass('active')){
-      $(this).addClass('active');
-      $(this).siblings().removeClass('active');
+  function showResults(url){
+    var template = Handlebars.compile($("#resultsTemplate").html());
+    $.getJSON(url, function(data){
+      $('#results').append(template(data));
+      console.log(data);
+    });
+    console.log("Funciono");
+  }
 
-      inputSearch_Album.addClass('active');
-      inputSearch_Album.siblings().removeClass('active');
+  inputSearch_Name.keypress(function(event){
+    if(event.keyCode == 13){
+      event.preventDefault();
+      $('.results .results-song').remove();
+
+      nameOfSearch = inputSearch_Name.val();
+      nameOfSearch = encodeURIComponent(nameOfSearch);
+
+      //En esta variable guardaré la nueva url
+      urlToFindSomething = "https://api.spotify.com/v1/search?q="+nameOfSearch+"&type="+valueOfOption+"";
+
+      console.log("Mi URL: " + urlToFindSomething);
+      console.log("Hola Enter");
+
+      showResults(urlToFindSomething);
     }
   });
-
-  listenOption_Track.on('click', function(){
-    if(!$(this).hasClass('active')){
-      $(this).addClass('active');
-      $(this).siblings().removeClass('active');
-
-      inputSearch_Track.addClass('active');
-      inputSearch_Track.siblings().removeClass('active');
-    }
-  });
-
-  listenOption_Playlist.on('click', function(){
-    if(!$(this).hasClass('active')){
-      $(this).addClass('active');
-      $(this).siblings().removeClass('active');
-
-      inputSearch_Playlist.addClass('active');
-      inputSearch_Playlist.siblings().removeClass('active');
-    }
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
